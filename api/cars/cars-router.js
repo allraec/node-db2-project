1209b38ea56b56330bcd9226134aaac7c37c0196
Allison Castaneda <allraec@yahoo.com>
2021-04-25 17:27:25 -0500

    get router done

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,13 @@
-// DO YOUR MAGIC
+const db = require("./cars-model");
+const router = require("express").Router();
+
+router.get('/', async (req, res, next) => {
+    try{
+        const cars = await db.getAll();
+        res.json(cars);
+    }catch (err){
+        next(err)
+    }
+})
+
+module.exports = router;
